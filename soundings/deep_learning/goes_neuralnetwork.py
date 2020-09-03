2d9b7f8779c915b4c60a093661e0171f82ac606c
stockeh <jason12stock@gmail.com>
2020-09-03 18:39:26 +0000

    fixed typo... roab to raob for training with rtma/goes

--- soundings/deep_learning/goes_neuralnetwork.py
+++ soundings/deep_learning/goes_neuralnetwork.py
@@ -7,11 +7,12 @@ import tensorflow as tf
 
 from soundings.deep_learning import callbacks
 
+
 class GOESNeuralNetwork():
-    def __init__(self, n_goes_inputs, n_rap_inputs, n_outputs, seed=None):
+    def __init__(self, n_goes_inputs, n_rap_inputs, goes_hiddens_list, n_outputs, activation='tanh', seed=None):
         
-        assert ((len(n_goes_inputs) == 3) and 
-                n_goes_inputs[-1] < n_goes_inputs[0]), 'GOES must be HxWxC dimensions'
+        assert ((len(n_goes_inputs) == 3)), f'GOES must be HxWxC dimensions, {n_goes_inputs}'
+        assert (isinstance(goes_hiddens_list, list)), f'{type(self).__name__}: goes_hiddens_list must be a list.'
         
         self.seed = seed
         self._set_seed()
@@ -19,26 +20,32 @@ class GOESNeuralNetwork():
 
         self.n_goes_inputs = n_goes_inputs
         self.n_rap_inputs = n_rap_inputs
+        self.goes_hiddens_list = goes_hiddens_list
         self.n_outputs = n_outputs
         
-        X1 = Z = tf.keras.Input(shape=n_goes_inputs, name='goes')
-        Z = tf.keras.layers.Conv2D(10, kernel_size=(3,3), strides=1,
-                                   activation='tanh', padding='same')(Z)
-        Z = tf.keras.layers.MaxPooling2D(pool_size=(2,2))(Z)
-        Z = tf.keras.layers.Conv2D(10, kernel_size=(3,3), strides=1,
-                                   activation='tanh', padding='same')(Z)
-        Z = tf.keras.layers.MaxPooling2D(pool_size=(2,2))(Z)
+        X1 = tf.keras.Input(shape=n_goes_inputs, name='goes')
         
-        out = tf.keras.layers.Dense(n_rap_inputs[0], activation='tanh')(tf.keras.layers.Flatten()(Z))
-        out = tf.keras.layers.Reshape((out.shape[1], 1))(out)
-
+        Z = tf.keras.layers.Flatten()(X1)
+        for units in goes_hiddens_list:
+            Z = tf.keras.layers.Dense(units, activation=activation)(Z)
+            
+        def pad_upper_zeros(z):
+            """Pad `n_rap_inputs`-previous units zeros to every tensor in the batch.
+            This isolates the information only near the surface.
+            """
+            prev_units = n_rap_inputs[0] - z.shape[1]
+            zeros =  tf.expand_dims(tf.zeros(prev_units), axis=0)
+            repeat = tf.squeeze(tf.keras.layers.RepeatVector(tf.shape(z)[0])(zeros), axis=0)
+            return tf.expand_dims(tf.concat([z, repeat], axis=1), axis=-1)
+     
+        out = tf.keras.layers.Lambda(pad_upper_zeros)(Z)
+        
         X2 = tf.keras.Input(shape=n_rap_inputs, name='rap')
         Z = tf.keras.layers.Concatenate(axis=2)([X2, out])
 
-        Z = tf.keras.layers.Conv1D(1, kernel_size=10, strides=1, activation='tanh', padding='same')(Z)
+        Z = tf.keras.layers.Conv1D(16, kernel_size=10, strides=1, activation='tanh', padding='same')(Z)
         Z = tf.keras.layers.MaxPooling1D(pool_size=2)(Z)
         Y = tf.keras.layers.Dense(n_outputs, name='out')(tf.keras.layers.Flatten()(Z))
-
         self.model = tf.keras.Model(inputs=[X1, X2], outputs=Y)
         
         self.GOESmeans = None
@@ -64,7 +71,7 @@ class GOESNeuralNetwork():
             np.random.seed(self.seed)
             random.seed(self.seed)
             tf.random.set_seed(self.seed)
-
+            
     def _setup_standardize(self, goes, rap, raob):
         if self.GOESmeans is None:
             self.GOESmeans = goes.mean(axis=0)
@@ -72,14 +79,14 @@ class GOESNeuralNetwork():
             self.GOESconstant = self.GOESstds == 0
             self.GOESstdsFixed = copy.copy(self.GOESstds)
             self.GOESstdsFixed[self.GOESconstant] = 1
-
+       
         if self.RAPmeans is None:
             self.RAPmeans = rap.mean(axis=0)
             self.RAPstds = rap.std(axis=0)
             self.RAPconstant = self.RAPstds == 0
             self.RAPstdsFixed = copy.copy(self.RAPstds)
             self.RAPstdsFixed[self.RAPconstant] = 1
-            
+
         if self.RAOBmeans is None:
             self.RAOBmeans = raob.mean(axis=0)
             self.RAOBstds = raob.std(axis=0)
@@ -94,7 +101,7 @@ class GOESNeuralNetwork():
 
     def _unstandardizeGOES(self, goes):
         return self.GOESstds * goes + self.GOESmeans
-
+            
     def _standardizeRAP(self, rap):
         result = (rap - self.RAPmeans) / self.RAPstdsFixed
         result[:, self.RAPconstant] = 0.0
@@ -120,7 +127,7 @@ class GOESNeuralNetwork():
         
         goes = self._standardizeGOES(goes)
         rap  = self._standardizeRAP(rap)
-        roab = self._standardizeRAOB(raob)
+        raob = self._standardizeRAOB(raob)
         
         if validation is not None:
             try:
@@ -151,11 +158,22 @@ class GOESNeuralNetwork():
         self.training_time = time.time() - start_time
         return self
 
-    def use(self, goes, rap):
+    def use(self, X):
+        """
+        Inputs:
+            X : {'goes': goes, 'rap': rap}
+        """
         # Set to error logging after model is trained
         tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-        goes = self._standardizeGOES(goes)
-        rap  = self._standardizeRAP(rap)
-#         Y = self.model.predict({'goes': goes, 'rap': rap})
+        goes = self._standardizeGOES(X['goes'])
+        rap  = self._standardizeRAP(X['rap'])
         Y = self._unstandardizeRAOB(self.model.predict({'goes': goes, 'rap': rap}))
         return Y
+        
+    def save(self, path):
+        self.model.save(path)
+        del self.model
+        with open(path + '/class.pickle', 'wb') as f:
+            pickle.dump(self, f)
+        self.model = tf.keras.models.load_model(path)
+
